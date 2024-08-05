e5f4386d67e63bd969faf811e710d7d64b691051
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-05 18:27:23 -0300

    React Componentization Challenge

--- 02_React_tools/src/main.jsx
+++ 02_React_tools/src/main.jsx
@@ -6,5 +6,6 @@ import Exerciciotwo from "./Exerciciotwo.jsx";
 import ClickEvents from "./Events.jsx";
 import Components from "./Components.jsx";
 import Properties from "./Properties.jsx";
+import Desafio from "./Desafio/Desafio.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Properties/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<Desafio/>);

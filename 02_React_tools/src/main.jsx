0bd253ff6b14e73cd472c6788630a422c5986023
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-05 16:53:39 -0300

    resolving exercices

--- 02_React_tools/src/main.jsx
+++ 02_React_tools/src/main.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Exercicio from "./Exercicio.jsx";
+import Exerciciotwo from "./Exerciciotwo.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Exercicio />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Exerciciotwo />);
